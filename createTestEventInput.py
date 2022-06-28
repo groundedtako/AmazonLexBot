bb5a8f66a5626b87bfc2d42fd8a3d7d4bef05952
Kelvin Hui <kwokhui@seas.upenn.edu>
2022-06-28 00:56:44 -0400

    added createTestEventInput

---
+++ createTestEventInput.py
@@ -0,0 +1,79 @@
+import json
+import re
+
+
+def create_slot_dict(value: str) -> dict:
+    """Helper function to create slot's value
+
+    Args:
+        value (str): value for that slot
+
+    Returns:
+        dict: resulting dictionary based on args
+
+    Todo:
+        -May want to include multiple values
+    """
+
+    return {
+        "shape": "Scalar",
+        "value": {
+            "originalValue": value,
+            "resolvedValues": [value],
+            "interpretedValue": value
+        }
+    }
+
+
+def create_test_event(resource: str, intent_name: str, slots_pairs: dict) -> dict:
+    """Function mimic Amazon Lex event that is passed to a Lambda Function
+
+    Args:
+        resource (str): resource name that want to test (Ex. EC2 / DynamoDB / S3)
+        intent_name (str): intent name for given resource (It should follow this regex pattern
+                           "(<resource>[a-zA-Z0-9]+)(<action>[a-zA-Z0-9]+)Intent"
+                           Ex. DynamoDBCreateTableIntent
+        slots_pairs (dict): key-value pairs for given intent (Exclude resource / action)
+
+    Returns:
+        dict: resulting test event input based on args
+              remember to remove ' ' 
+    """
+
+    slots = {}
+    slots["resource"] = create_slot_dict(resource)
+
+    regex_pattern = f'{resource}(.*)Intent'
+    action = re.search(regex_pattern, intent_name).group(1)
+
+    slots[f"{resource}_action"] = create_slot_dict(action)
+
+    for k, v in slots_pairs.items():
+        slots[k] = create_slot_dict(v)
+
+    TestEventInput = {
+        "sessionId": "279344343543764",
+        "sessionState": {
+            "sessionAttributes": {},
+            "activeContexts": [
+                {
+                    "timeToLive": {
+                        "turnsToLive": 20,
+                        "timeToLiveInSeconds": 600
+                    },
+                    "name": "contextWelcome",
+                    "contextAttributes": {
+                        "resource": resource
+                    }
+                }
+            ],
+            "intent": {
+                "slots": slots,
+                "confirmationState": "None",
+                "name": intent_name,
+                "state": "ReadyForFulfillment"
+            }
+        }
+    }
+
+    return json.dumps(TestEventInput)
