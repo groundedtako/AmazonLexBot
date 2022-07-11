1f8e427e02ed6c41e75dfe41338455289b0e9429
grounded-tako <sweetoctopus@outlook.com>
2022-07-11 14:39:44 -0700

    test S3 events

--- Aws_Lambda/testHandleS3Event.py
+++ Aws_Lambda/testHandleS3Event.py
@@ -10,20 +10,46 @@ from createTestEventInput import *
 
 @mock_s3
 class TestMockClassLevel(unittest.TestCase):
-    def setUp(self):
-        s3_client = boto3.client("s3", region_name="us-east-1")
+    def __init__(self):
+        self.s3_client = boto3.client("s3", region_name="us-east-1")
 
-    def test_creating_a_bucket(self):
-        # 'mybucket', created in setUp, is accessible in this test
-        # Other clients can be created at will
+    def test_s3_create_a_bucket(self):
 
-        s3_client = boto3.client("s3", region_name="us-east-1")
+        # case 1: creating a bucket with valid parameters
         intent_request = create_test_event(resource="S3", intent_name="S3CreateBucketIntent", slots_pairs=["CreateBucket", "testBucket", "test"])
-        bucket = s3_create_bucket(s3_client, intent_request)
+        bucket = s3_create_bucket(self.s3_client, intent_request)
 
         assert bucket == False
+
+        # case 2: creating a bucket with existing parameters
+
+
+    def test_s3_put_object(self):
+
+        # case 1: test putting valid object in bucket
+
+        pass
+
+    def test_s3_delete_object(self):
+
+        # case 1: test deleting existing object in bucket
+
+        # case 2: test deleting non-existing object in bucket
+
+        # case 3: test deleting existing object in non-existing bucket
+
+        pass
+
+    def test_s3_delete_bucket(self):
+
+        # case 1: test deleting empty existing bucket
+
+        # case 2: test deleting non-empty existing bucket
+
+        # case 3: test deleting non-existing bucket
+
+        pass
 
 
 if __name__ == "__main__":
     test = TestMockClassLevel()
-    test.test_creating_a_bucket()
